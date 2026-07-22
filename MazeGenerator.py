b4f935175e1d0a555d72b5ca210569ea308d7b44
ELias <elias2005new@gmail.com>
2026-07-22 12:18:20 +0300

    imperfct in done

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -15,10 +15,37 @@ class MazeGenerator():
         self.entry_y = entry_y
         self.exit_x = exit_x
         self.exit_y = exit_y
+    
+    def pattern_42(self) -> list[int]:
+        self.point_42 = []
+        if self.width > 9 and self.height > 7:
+            x = self.width // 2
+            y = self.height // 2
+            self.point_42.append((x - 1, y))
+            self.point_42.append((x - 2, y))
+            self.point_42.append((x - 3, y))
+            self.point_42.append((x - 3, y - 1))
+            self.point_42.append((x - 3, y - 2))
+            self.point_42.append((x - 1, y + 1))
+            self.point_42.append((x - 1, y + 2))
+            self.point_42.append((x + 1, y))
+            self.point_42.append((x + 2, y))
+            self.point_42.append((x + 3, y))
+            self.point_42.append((x + 3, y - 1))
+            self.point_42.append((x + 3, y - 2))
+            self.point_42.append((x + 2, y - 2))
+            self.point_42.append((x + 1, y - 2))
+            self.point_42.append((x + 1, y + 1))
+            self.point_42.append((x + 1, y + 2))
+            self.point_42.append((x + 2, y + 2))
+            self.point_42.append((x + 3, y + 2))
+        return self.point_42
+
 
 
     def back_trackinga_agorithm(self) -> list[int]: #width and height index error fix it
-        direction = {"N": 1, "E": 8, "S": 4, "W": 2}
+        direction = {"N": 1, "E": 2, "S": 4, "W": 8}
+        skipped_point = self.pattern_42()
         self.arr = [[15 for i in range(self.width)] for j in range(self.height)]
         print(self.arr)
         self.num_cells = self.width * self.height
@@ -30,16 +57,20 @@ class MazeGenerator():
             x, y = self.visited_cells[-1]
             if x >= 1:
                 if self.arr[y][x - 1] == 15:
-                    self.next_cell.append(((x - 1), y))
+                    if (x - 1, y) not in skipped_point:
+                        self.next_cell.append(((x - 1), y))
             if x < self.width - 1:
                 if self.arr[y][x + 1] == 15:
-                    self.next_cell.append(((x + 1), y))
+                    if (x + 1, y) not in skipped_point:
+                        self.next_cell.append(((x + 1), y))
             if y >= 1:
                 if self.arr[y - 1][x] == 15:
-                    self.next_cell.append((x, (y - 1)))
+                    if (x, y - 1) not in skipped_point:
+                        self.next_cell.append((x, (y - 1)))
             if y < self.height - 1:
                 if self.arr[y + 1][x] == 15:
-                    self.next_cell.append((x, (y + 1)))
+                    if (x, y + 1) not in skipped_point:
+                        self.next_cell.append((x, (y + 1)))
             if self.next_cell:
                 chosen = random.choice(self.next_cell)
                 x1, y1 = chosen
@@ -47,11 +78,11 @@ class MazeGenerator():
                 val = ((x - x1) , (y - y1))
                 print(val)
                 if val == (1, 0):
-                    self.arr[y][x] -= 2
-                    self.arr[y1][x1] -= 8
-                elif val == (-1, 0):
                     self.arr[y][x] -= 8
                     self.arr[y1][x1] -= 2
+                elif val == (-1, 0):
+                    self.arr[y][x] -= 2
+                    self.arr[y1][x1] -= 8
                 elif val == (0, 1):
                     self.arr[y][x] -= 1
                     self.arr[y1][x1] -= 4
@@ -61,8 +92,6 @@ class MazeGenerator():
                 self.visited_cells.append(chosen)
             else:
                 self.visited_cells.pop()
-        self.arr[self.entry_y][self.entry_x] -= 2
-        self.arr[self.exit_y][self.exit_x]  -= 8
         print(self.arr)
         return self.arr
     
@@ -73,64 +102,63 @@ class MazeGenerator():
         while length > 0:
             if (( #delete from S -> N
                 (self.arr[y - 1][x + 1] & 4) == 4 or (
-                    self.arr[y - 1][x + 1] & 2) == 2 or (
-                        self.arr[y][x + 1] & 2) == 2) and (
-                            (self.arr[y - 1][x - 1] & 8) == 8 or (
+                    self.arr[y - 1][x + 1] & 8) == 8 or (
+                        self.arr[y][x + 1] & 8) == 8) and (
+                            (self.arr[y - 1][x - 1] & 2) == 2 or (
                                 self.arr[y - 1][x - 1] & 4) == 4 or (
-                                    self.arr[y][x - 1] & 8) == 8) and (
+                                    self.arr[y][x - 1] & 2) == 2) and (
                                         (self.arr[y][x] & 1) == 1
                                     )
                     ):
                 if random.random() < 0.3:
-                    self.arr[y][x] -= 1
-                    self.arr[y - 1][x] -= 4
+                    if self.arr[y - 1][x] != 15 and self.arr[y][x] != 15:
+                        self.arr[y][x] -= 1
+                        self.arr[y - 1][x] -= 4
             if (( #delete from N -> S
-                (self.arr[y + 1][x + 1] & 2) == 2 or (
+                (self.arr[y + 1][x + 1] & 8) == 8 or (
                     self.arr[y + 1][x + 1] & 1) == 1 or (
-                        self.arr[y][x + 1] & 2) == 2) and (
-                            (self.arr[y + 1][x - 1] & 8) == 8 or (
-                                self.arr[y - 1][x - 1] & 1) == 1 or (
-                                    self.arr[y][x - 1] & 8) == 8) and (
+                        self.arr[y][x + 1] & 8) == 8) and (
+                            (self.arr[y + 1][x - 1] & 2) == 2 or (
+                                self.arr[y + 1][x - 1] & 1) == 1 or (
+                                    self.arr[y][x - 1] & 2) == 2) and (
                                         (self.arr[y][x] & 4) == 4
                                     )
                     ):
                 if random.random() < 0.3:
-                    self.arr[y][x] -= 4
-                    self.arr[y + 1][x] -= 1
+                    if self.arr[y + 1][x] != 15 and self.arr[y][x] != 15:
+                        self.arr[y + 1][x] -= 1
+                        self.arr[y][x] -= 4
             if (( # delete from W -> E
-                (self.arr[y - 1][x + 1] & 2) == 2 or (
+                (self.arr[y - 1][x + 1] & 8) == 8 or (
                     self.arr[y - 1][x + 1] & 4) == 4 or (
                         self.arr[y - 1][x] & 4) == 4) and (
                             (self.arr[y + 1][x + 1] & 1) == 1 or (
-                                self.arr[y + 1][x + 1] & 2) == 2 or (
-                                    self.arr[y + 1][x] & 1) == 1) and (
-                                        (self.arr[y][x] & 8) == 8
-                                    )
-                    ):
-                if random.random() < 0.3:
-                    self.arr[y][x] -= 8
-                    self.arr[y][x + 1] -= 2
-            if (( # delete from E -> W
-                (self.arr[y - 1][x - 1] & 8) == 8 or (
-                    self.arr[y - 1][x - 1] & 4) == 4 or (
-                        self.arr[y - 1][x] & 4) == 4) and (
-                            (self.arr[y + 1][x - 1] & 1) == 1 or (
-                                self.arr[y + 1][x - 1] & 8) == 8 or (
+                                self.arr[y + 1][x + 1] & 8) == 8 or (
                                     self.arr[y + 1][x] & 1) == 1) and (
                                         (self.arr[y][x] & 2) == 2
                                     )
                     ):
                 if random.random() < 0.3:
-                    self.arr[y][x] -= 2
-                    self.arr[y][x - 1] -= 8
+                    if self.arr[y][x + 1] != 15 and self.arr[y][x] != 15:
+                        self.arr[y][x] -= 2
+                        self.arr[y][x + 1] -= 8
+            if (( # delete from E -> W
+                (self.arr[y - 1][x - 1] & 2) == 2 or (
+                    self.arr[y - 1][x - 1] & 4) == 4 or (
+                        self.arr[y - 1][x] & 4) == 4) and (
+                            (self.arr[y + 1][x - 1] & 1) == 1 or (
+                                self.arr[y + 1][x - 1] & 2) == 2 or (
+                                    self.arr[y + 1][x] & 1) == 1) and (
+                                        (self.arr[y][x] & 8) == 8
+                                    )
+                    ):
+                if random.random() < 0.3:
+                    if self.arr[y][x - 1] != 15 and self.arr[y][x] != 15:
+                        self.arr[y][x] -= 8
+                        self.arr[y][x - 1] -= 2
             x += 1
             if x == self.width - 1:
                 y += 1
                 x = 1
             length -= 1
         return self.arr
-
-
-            
-
-
